601d80431c379be4eacfda5755e2694d2d51701c
Matt Read <mread@spotd.co.uk>
2013-03-29 23:55:28 +0000

    round-trip the json

--- app/controllers/QuickSetup.java
+++ app/controllers/QuickSetup.java
@@ -1,13 +1,11 @@
 package controllers;
 
-import play.data.Form;
+import org.codehaus.jackson.JsonNode;
 import play.data.validation.Constraints;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
 import views.html.quicksetup;
-
-import static play.data.Form.form;
 
 public class QuickSetup extends Controller {
 
@@ -15,8 +13,10 @@ public class QuickSetup extends Controller {
         return ok(quicksetup.render());
     }
 
-    public static Result addRoom() {
-        return ok();
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result create() {
+        JsonNode json = request().body().asJson();
+        return ok(json.toString());
     }
 
     public static class Room {
